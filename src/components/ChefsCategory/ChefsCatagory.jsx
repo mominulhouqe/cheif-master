cb47724b57fad529475e73515c7d7a58bde4db84
mominul <ulhouqemomin39@gmail.com>
2023-05-04 10:09:52 +0600

    added card

--- src/components/ChefsCategory/ChefsCatagory.jsx
+++ src/components/ChefsCategory/ChefsCatagory.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Chiefs from '../ChiefStaap/Chiefs';
 
 const ChefsCatagory = () => {
 
@@ -16,10 +17,10 @@ const ChefsCatagory = () => {
     <div className='bg-orange-100 p-10 container mx-auto'>
       <div className='text-center text-orange-500 font-mono font-extrabold mb-10'>
         <h1 className='mb-5'>OUR POPULAR CHIEF</h1> <hr />
-        <p>Best Category Foods Menu {users.id}</p>
+        <p>Best Category Foods Menu </p>
      
       </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
+      {/* <div className="card w-96 bg-base-100 shadow-xl">
         <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
         <div className="card-body">
           <h2 className="card-title">Shoes!</h2>
@@ -28,7 +29,16 @@ const ChefsCatagory = () => {
             <button className="btn btn-primary">Deatils</button>
           </div>
         </div>
+      </div> */}
+   <div className="grid grid-cols-3 gap-10">
+        {users.map(user => (
+          <Chiefs
+            key={user.id}
+            user={user}
+          />
+        ))}
       </div>
+
     </div>
   );
 };
